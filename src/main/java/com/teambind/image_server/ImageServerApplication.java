9eadc28aa3afb29346a8d5b5004432e0bbb053b5
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-18 19:25:50 +0900

    [FEAT] 확장자 검증 로직 및 초기 설정 추가
    src/main/java/com/teambind/image_server/ImageServerApplication.java: 확장자 데이터를 캐싱하는 `ApplicationRunner` 구현 추가
    src/main/java/com/teambind/image_server/config/InitialConfig.java: 빈 설정을 위한 초기 구성 클래스 추가
    src/main/java/com/teambind/image_server/util/validator/ExtensionParser.java: 확장자 파싱 유틸리티 클래스 추가
    src/main/java/com/teambind/image_server/util/validator/ExtensionValidator.java: 파일 확장자 검증 클래스 추가
    
    참고: 확장자는 `ExtensionRepository`를 통해 DB에서 로드됩니다.
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/image_server/ImageServerApplication.java
+++ src/main/java/com/teambind/image_server/ImageServerApplication.java
@@ -1,13 +1,33 @@
 package com.teambind.image_server;
 
+import com.teambind.image_server.repository.ExtensionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ImageServerApplication {
+import java.util.HashMap;
+import java.util.Map;
 
+@SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
+
+public class ImageServerApplication implements ApplicationRunner {
+
+    public static final Map<String, Integer> extensionMap = new HashMap<>();
+    private final ExtensionRepository extensionRepository;
     public static void main(String[] args) {
         SpringApplication.run(ImageServerApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        extensionRepository.findAll().forEach(extension -> {
+            extensionMap.put(extension.getCode(), extension.getId());
+        });
+        log.info("Loaded {} extensions", extensionMap.toString());
+    }
 }
