64af21cc5c9f934e47441d4cfe09e94ed1ea2339
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-18 19:30:58 +0900

    [FEAT] WebP 변환기 및 예외 처리 클래스 추가
    src/main/resources/Application-dev.yaml: WebP 이미지 디렉토리 설정 추가
    src/main/java/com/teambind/image_server/util/convertor/WebpWriter.java: WebP 이미지 작성 클래스 구현
    src/main/java/com/teambind/image_server/util/convertor/WebpConvertor.java: WebP 파일 변환기 구현
    src/main/java/com/teambind/image_server/exception/ImageException.java: 이미지 변환 관련 예외 처리 클래스 추가
    
    참고: WebP 변환 기능은 Scrimage 라이브러리를 활용합니다.
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

---
+++ src/main/java/com/teambind/image_server/exception/ImageException.java
@@ -0,0 +1,7 @@
+package com.teambind.image_server.exception;
+
+public class ImageException extends RuntimeException {
+    public ImageException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/teambind/image_server/util/convertor/WebpConvertor.java
@@ -0,0 +1,38 @@
+package com.teambind.image_server.util.convertor;
+
+import com.sksamuel.scrimage.ImmutableImage;
+import com.sksamuel.scrimage.webp.WebpWriter;
+import com.teambind.image_server.exception.ImageException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+public class WebpConvertor {
+
+    @Value("${image.dir}")
+    private String PHOTO_DIR;
+
+    public File convertToWebp(String fileName, File originalFile) {
+        try {
+            return ImmutableImage.loader()// 라이브러리 객체 생성
+                    .fromFile(originalFile) // .jpg or .png File 가져옴
+                    .output(WebpWriter.DEFAULT, new File(PHOTO_DIR + fileName + ".webp")); // 손실 압축 설정, fileName.webp로 파일 생성
+        } catch (Exception e) {
+            //TODO
+            throw new ImageException(e.getMessage());
+        }
+    }
+
+    public File convertToWebpWithLossless(String fileName, File originalFile) {
+        try {
+            return ImmutableImage.loader()// 라이브러리 객체 생성
+                    .fromFile(originalFile) // .jpg or .png File 가져옴
+                    .output(WebpWriter.DEFAULT.withLossless(), new File(PHOTO_DIR + fileName + ".webp")); // 무손실 압축 설정, fileName.webp로 파일 생성
+        } catch (Exception e) {
+            //TODO
+            throw new ImageException(e.getMessage());
+        }
+    }
+}
