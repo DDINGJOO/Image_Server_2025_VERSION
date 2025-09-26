72587621e40b00d175b8558bc1f5c3df80e2856f
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-26 17:42:26 +0900

    [FEAT] 운영 환경 개선 및 이미지 저장 컨트롤러 업데이트
    .env.prod: Kafka 클러스터 URL 및 BASE_URL 환경 변수 수정/추가
    src/main/resources/application-prod.yaml: BASE_URL 환경 변수 적용
    docker-compose.yml: Redis와 MariaDB 구성 제거, 외부 네트워크 환경 구성 추가, 이미지 서버 버전 업데이트 (1.6 → 1.8)
    src/main/java/com/teambind/image_server/controller/ImageSaveController.java: 다중 및 단일 이미지 저장 엔드포인트에 MediaType 적용
    Dockerfile
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/image_server/controller/ImageSaveController.java
+++ src/main/java/com/teambind/image_server/controller/ImageSaveController.java
@@ -5,6 +5,7 @@ import com.teambind.image_server.entity.Image;
 import com.teambind.image_server.exception.CustomException;
 import com.teambind.image_server.service.ImageSaveService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,15 +22,15 @@ import java.util.List;
 public class ImageSaveController {
     private final ImageSaveService imageSaveService;
 
-    @PostMapping
-    public ResponseEntity<String> saveImage(@RequestParam MultipartFile file, String referenceId, String uploaderId, String category) throws CustomException {
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> saveImage(@RequestParam("file") MultipartFile file, @RequestParam String referenceId, @RequestParam String uploaderId, @RequestParam String category) throws CustomException {
         Image image = imageSaveService.saveImage(file, referenceId, uploaderId, category);
         return ResponseEntity.ok().body(image.getId());
     }
 
-    @PostMapping("/batch")
-    public ResponseEntity<List<String>> saveImage(@RequestParam List<MultipartFile> file, String referenceId, String uploaderId, String category) throws CustomException {
-        List<Image> image = imageSaveService.saveImages(file, referenceId, uploaderId, category);
+    @PostMapping(path = "/batch", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<List<String>> saveImages(@RequestParam("files") List<MultipartFile> files, @RequestParam String referenceId, @RequestParam String uploaderId, @RequestParam String category) throws CustomException {
+        List<Image> image = imageSaveService.saveImages(files, referenceId, uploaderId, category);
         List<String> imageIds = new ArrayList<>();
         for (Image img : image) {
             imageIds.add(img.getId());
