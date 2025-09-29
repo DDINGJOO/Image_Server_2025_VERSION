6a4216c542c4e25d75c81f7d1f8dda89807de7f1
ddingjoo <ddingsha9@teambind.co.kr>
2025-09-29 16:22:20 +0900

    [REFACTOR] Enums API 반환값을 ResponseEntity로 변경
    src/main/java/com/teambind/image_server/controller/enumsController.java: 확장자 및 참조 타입을 문자열 Map 형식으로 변환하여 ResponseEntity로 반환하도록 수정
    
    참고: 반환값 변경에 따른 프론트엔드 영향 점검 필요.
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/image_server/controller/enumsController.java
+++ src/main/java/com/teambind/image_server/controller/enumsController.java
@@ -3,10 +3,12 @@ package com.teambind.image_server.controller;
 
 import com.teambind.image_server.entity.Extension;
 import com.teambind.image_server.entity.ReferenceType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.teambind.image_server.ImageServerApplication.extensionMap;
@@ -16,12 +18,21 @@ import static com.teambind.image_server.ImageServerApplication.referenceTypeMap;
 @RequestMapping("/api/enums")
 public class enumsController {
     @GetMapping("/extensions")
-    public Map<String, Extension> getEnums() {
-        return extensionMap;
+
+    public ResponseEntity<Map<String, String>> getEnums() {
+        Map<String, String> map = new HashMap<>();
+        for (Map.Entry<String, Extension> entry : extensionMap.entrySet()) {
+            map.put(entry.getKey(), entry.getValue().getName());
+        }
+        return ResponseEntity.ok(map);
     }
 
     @GetMapping("/referenceType")
-    public Map<String, ReferenceType> getReferenceTypes() {
-        return referenceTypeMap;
+    public ResponseEntity<Map<String, String>> getReferenceTypes() {
+        Map<String, String> map = new HashMap<>();
+        for (Map.Entry<String, ReferenceType> entry : referenceTypeMap.entrySet()) {
+            map.put(entry.getKey(), entry.getValue().getName());
+        }
+        return ResponseEntity.ok(map);
     }
 }
