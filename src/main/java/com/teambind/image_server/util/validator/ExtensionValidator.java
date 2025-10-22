92ad38e5cbc1a89787d36a9ea938ab70daeb984e
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-22 23:20:06 +0900

    [FEAT] 확장자 유효성 검사에 InitialSetup 데이터 매핑 적용
    src/main/java/com/teambind/image_server/util/validator/ExtensionValidator.java: EXTENSION_MAP을 사용하여 확장자 유효성 검증 로직 업데이트
    
    참고: InitialSetup 설정값 관련 추가 테스트 필요.
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/image_server/util/validator/ExtensionValidator.java
+++ src/main/java/com/teambind/image_server/util/validator/ExtensionValidator.java
@@ -1,19 +1,19 @@
 package com.teambind.image_server.util.validator;
 
+import com.teambind.image_server.util.InitialSetup;
 import com.teambind.image_server.util.helper.ExtensionParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import static com.teambind.image_server.ImageServerApplication.extensionMap;
 
 @Component
 @RequiredArgsConstructor
 public class ExtensionValidator {
 	private final ExtensionParser parser;
 	
+	
 	public boolean isValid(String fileName) {
 		String extension = parser.extensionParse(fileName);
-		return extensionMap.containsKey(extension);
+		return InitialSetup.EXTENSION_MAP.containsKey(extension);
 	}
 	
 	
